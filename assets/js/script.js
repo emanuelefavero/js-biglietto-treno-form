981c38ff02c79c342b3e549a006ab2c3e7848844
Emanuele Favero <emafaux89@gmail.com>
2026-05-29 02:01:25 +0200

    feat(milestone 1): add form, inputs and log values to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log('hi');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  const km = Number(formData.get('km'));
+  const age = Number(formData.get('age'));
+
+  console.log(km, age);
+});
